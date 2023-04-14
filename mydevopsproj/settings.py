d66453063fc694c6ee8dde6291d6388eabc8d6c9
EC2 Default User <ec2-user@ip-172-31-18-54.eu-west-1.compute.internal>
2023-04-14 19:50:06 +0000

    functinality check commit1

--- mydevopsproj/settings.py
+++ mydevopsproj/settings.py
@@ -69,7 +69,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'mydevopsproj.wsgi.application'
-WSGIPassAuthorization = 'On'
+
 
 
 # Database
@@ -78,8 +78,7 @@ WSGIPassAuthorization = 'On'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': OS.path.join(BASE_DIR, 'db.sqlite3'),
-        #'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
